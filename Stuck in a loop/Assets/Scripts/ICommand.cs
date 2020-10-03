832141b3223c4bb26358f4a918e9379c09af02c0
Tayfun Gürlevik <tgurlevik@gmail.com>
2020-10-03 12:53:15 +0300

    first commit
    Coroutine bugs

---
+++ Stuck in a loop/Assets/Scripts/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public  interface ICommand
+{
+    
+    int Index { get; set; }
+    int Amount { get; set; }
+    bool CommandTurn { get; set; }
+    IEnumerator Execute();
+}
---
+++ Stuck in a loop/Assets/Scripts/RobotController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public class RobotController : MonoBehaviour
+{
+
+    private List<ICommand> commands;
+
+    private void Start()
+    {
+        
+    }
+    public void PlayRobot()
+    {
+        TakeCommands();
+        StartCoroutine( ExecuteCommands ());
+    }
+    private IEnumerator ExecuteCommands()
+    {
+        var orderedCommands = commands.OrderBy(t => t.Index);
+
+        foreach (var item in orderedCommands)
+        {
+            item.CommandTurn = true;
+            StartCoroutine(item.Execute());
+            yield return new WaitForSeconds(3.0f);
+            StopCoroutine(item.Execute());
+        }
+    }
+
+    private void TakeCommands()
+    {
+        commands = new List<ICommand>();
+        var moveCommands = FindObjectsOfType<MoveCommand>().ToList<ICommand>();
+        var turnCommands = FindObjectsOfType<TurnCommand>().ToList<ICommand>();
+        commands.AddRange(moveCommands);
+        commands.AddRange(turnCommands);
+            
+    }
+}
